cd8d9cb74703bebee7230a968a46c571183b4385
Ashish Mittal <ashishm@jcsoftwaresolution.com>
2024-09-20 17:10:34 +0530

    Simplify material inside invoices
    Changed it to Array type

--- app/models/invoices.model.js
+++ app/models/invoices.model.js
@@ -4,63 +4,7 @@ var Mongoose = require("mongoose"),
 
 
 module.exports = (mongoose) => {
-    const categoryDetailSchema = mongoose.Schema({
-        _id: { type: Schema.Types.ObjectId },
-        name: { type: String },
-        slug: { type: String },
-        image: { type: String },
-        type: { type: String },
-        status: { type: String },
-        addedBy: { type: Schema.Types.ObjectId },
-        isDeleted: { type: Boolean, default: false },
-        createdAt: { type: Date },
-        updatedAt: { type: Date }
-      });
-      
-    const supplierDetailSchema = mongoose.Schema({
-    _id: { type: Schema.Types.ObjectId },
-    firstName: { type: String },
-    lastName: { type: String },
-    fullName: { type: String },
-    email: { type: String },
-    mobileNo: { type: String },
-    address: { type: String },
-    address2: { type: String },
-    company: { type: String },
-    zipCode: { type: String },
-    state: { type: String },
-    country: { type: String },
-    skills: [ {type: Schema.Types.ObjectId} ],
-    isVerified: { type: String},
-    status: { type: String },
-    addedBy: { type: Schema.Types.ObjectId },
-    isDeleted: { type: Boolean, default: false },
-    createdAt: { type: Date },
-    updatedAt: { type: Date }
-    });
     
-    const materialSchema = mongoose.Schema({
-    _id: { type: Schema.Types.ObjectId },
-    id: { type: String},
-    name: { type: String },
-    image: { type: String },
-    category: { type: Schema.Types.ObjectId },
-    price: { type: Number },
-    quantity: { type: Number },
-    unit: { type: String },
-    vat_included: { type: Boolean },
-    vat: { type: Number },
-    status: { type: String },
-    createdAt: { type: Date },
-    updatedAt: { type: Date},
-    isDeleted: { type: Boolean, default: false },
-    addedBy: { type: Schema.Types.ObjectId },
-    addedByName: { type: String },
-    category_detail: categoryDetailSchema, 
-    supplier_detail: supplierDetailSchema 
-    });
-
-
     var schema = mongoose.Schema(
         {
             jobId: { type: Schema.Types.ObjectId, ref: "jobs", },
@@ -78,7 +22,7 @@ module.exports = (mongoose) => {
             isDeleted: { type: Boolean, default: false },
             createdAt: Date,
             updatedAt: Date,
-            material: [materialSchema],
+            material: {type: Array},
             servicefee: { type: Number, default: 0 }
         },
         { timestamps: true }
