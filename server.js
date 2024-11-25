b346b8a0a11acdf320a2aa82e0dd69e6f0dd26ba
Ashish Mittal <ashishm@jcsoftwaresolution.com>
2024-11-25 14:40:19 +0530

    Set payload size limit to 500mb

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ let http = require("http");
 var bcrypt = require('bcrypt');
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "500mb" }));
 
 app.use(express.urlencoded({ extended: true }));
 //server static files
